45f60d966e84a5917b7234ee1c1f774f4440c1d7
Jared Harbison <jared.harbison@gmail.com>
2019-04-27 14:58:37 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -1,16 +1,18 @@
 function displayRepositories() {
     var repos = JSON.parse(this.responseText);
+    // console.log(this.responseText);
     const repoList = `<ul>${
         repos.map(
-            r => 
+            r =>
                 '<li>' + 
                 r.name + 
-                ' - <a href= "' + 
-                r.html_url + 
+                ' - <a href= "' +
+                r.html_url +
                 '" data-repo = "' +
                 r.name +
                 '" onclick="getCommits(this)">Get Commits</a></li>'
                 )
         .join('')}</ul>`;
+
     document.getElementById("repositories").innerHTML = repoList;
 }
