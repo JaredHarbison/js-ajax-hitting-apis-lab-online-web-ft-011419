f31e082ce335dffca24ce2aaa5dad7d522d5bba2
Jared Harbison <jared.harbison@gmail.com>
2019-04-27 15:01:50 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -16,5 +16,18 @@ function displayRepositories() {
 }
 
 function displayCommits() {
-    
+    const commits = JSON.parse(this.responseText);
+    const commitsList = `<ul>${
+        commits.map(
+            commit =>
+                '<li><strong>' + 
+                commit.author.login + 
+                '</strong> - ' +
+                commit.commit.author.name + 
+                ' - ' + 
+                commit.commit.message + 
+                '</li>'
+        )
+        .join('')}</ul>`;
+    document.getElementById('details').innerHTML = commitsList;
 }
