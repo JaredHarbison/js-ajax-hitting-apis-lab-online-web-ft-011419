5ca5f1134632dd2aa44b981501a8f4c70635e9a5
Jared Harbison <jared.harbison@gmail.com>
2019-04-27 15:07:10 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -48,8 +48,8 @@ function displayBranches() {
 }
 
 function getRepositories() {
-    const req = new XMLHttpRequest() 
-    const user = document.getElementById("username").value 
+    const req = new XMLHttpRequest()
+    const user = document.getElementById("username").value
     req.addEventListener("load", displayRepositories)
     req.open("GET", `https://api.github.com/users/${user}/repos`)
     req.send()
