3d6509ae8fa68159ebe866e0fd4f673792c86e9c
Jared Harbison <jared.harbison@gmail.com>
2019-04-27 14:57:01 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -1,1 +1,14 @@
-// your code here
+function displayRepositories() {
+    var repos = JSON.parse(this.responseText);
+    const repoList = `<ul>${
+        repos.map(
+            r => 
+                '<li>' + 
+                r.name + 
+                ' - <a href= "' + 
+                r.html_url + 
+                '" data-repo = "' +
+                r.name +
+                '" onclick="getCommits(this)">Get Commits</a>
+      )
+}
